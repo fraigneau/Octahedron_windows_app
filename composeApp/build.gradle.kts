12209fe637380d6482aa82f53e20656884c978cf
fraigneau <fraigneaulucas@proton.me>
2025-10-16 02:36:32 +0200

    feat(build): add Kotlin serialization plugin and update package details

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.composeHotReload)
+    alias(libs.plugins.kotlinSerialization)
 }
 
 kotlin {
@@ -20,6 +21,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.kotlinx.serialization.json)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -38,8 +40,9 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "com.octahedron"
-            packageVersion = "1.0.0"
+            packageName = "octahedron"
+            vendor = "octahedron"
+            packageVersion = "1.0.1.1"
         }
     }
 }
