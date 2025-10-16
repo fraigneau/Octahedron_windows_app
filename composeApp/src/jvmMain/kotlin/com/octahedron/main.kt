f55a5fa8e1039d54aa52e9ea34a6a2bd50634c7d
fraigneau <fraigneaulucas@proton.me>
2025-10-16 18:45:18 +0200

    feat(ui): enhance main application window with tray support and Material3 theme

--- composeApp/src/jvmMain/kotlin/com/octahedron/main.kt
+++ composeApp/src/jvmMain/kotlin/com/octahedron/main.kt
@@ -1,14 +1,45 @@
 package com.octahedron
 
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.window.*
 import com.octahedron.ui.App
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 fun main() = application {
+
+    val windowState = rememberWindowState(
+        placement = WindowPlacement.Floating,
+        position = WindowPosition.Aligned(Alignment.Center),
+        size = DpSize(1000.dp, 700.dp)
+    )
+
+    val appIcon = painterResource("logo.png")
+
+    val trayState = rememberTrayState()
+    Tray(
+        state = trayState,
+        icon = appIcon,
+        tooltip = "Octahedron",
+    )
+
     Window(
         onCloseRequest = ::exitApplication,
-        title = "octahedron",
+        state = windowState,
+        title = "Octahedron",
+        icon = appIcon,
+        resizable = true
     ) {
-        App()
+        MaterialTheme(colorScheme = darkColorScheme()) {
+            Surface(Modifier.fillMaxSize()) {
+                App()
+            }
+        }
     }
 }
